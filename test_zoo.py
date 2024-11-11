648b40329c8962c18c78ef711e2399738c9173b7
Kanyanat-Uetrakulchai <kanyanat.uet@student.mahidol.ac.th>
2024-11-11 05:32:41 +0000

    Bugs were fixed

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,44 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-5), "Invalid")
+
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid")
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(6), 50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+    
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_middleage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_middleage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(48), 150)
+
+    def test_middleage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100)
 
 if __name__ == '__main__':
     unittest.main()
